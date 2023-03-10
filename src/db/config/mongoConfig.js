fa1da54b26b2139b91407cbfb3107b39418a321d
IBadella <ignacio.badella@ayi.group>
2023-03-10 13:04:12 -0300

    mongo config

--- src/db/config/mongoConfig.js
+++ src/db/config/mongoConfig.js
@@ -6,10 +6,11 @@ config();
 export async function mongoConnect() {
     try {
         //const URL = "mongodb+srv://IBadella:yR6NsdkTtzIObQcF@nachocluster.ytxqvsv.mongodb.net/?retryWrites=true&w=majority"
-        let res = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        let res = await mongoose.connect('mongodb+srv://IBadella:yR6NsdkTtzIObQcF@nachocluster.ytxqvsv.mongodb.net/?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
         console.log('MongoDB connected');
     } catch (e) {
         throw new Error(`Error while connecting to Mongo DB ${e}`)
