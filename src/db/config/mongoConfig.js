d111040a3f7c28279e649a7f6098e16171d2a7e0
IBadella <ignacio.badella@ayi.group>
2023-03-11 11:42:41 -0300

    mongodb config

--- src/db/config/mongoConfig.js
+++ src/db/config/mongoConfig.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 //import { configObject } from "./index.js";
-import { config } from "dotenv";
-config();
+//import { config } from "dotenv";
+//config();
 
 export async function mongoConnect() {
     try {
-        //const URL = "mongodb+srv://IBadella:yR6NsdkTtzIObQcF@nachocluster.ytxqvsv.mongodb.net/test?retryWrites=true&w=majority"
+        //const URL = "mongodb+srv://IBadella:yR6NsdkTtzIObQcF@nachocluster.ytxqvsv.mongodb.net/?retryWrites=true&w=majority"
         let res = await mongoose.connect(process.env.MONGO_URL,
             {
                 useNewUrlParser: true,
